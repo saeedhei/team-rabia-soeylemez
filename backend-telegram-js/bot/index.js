abd182bbffbebca87cbe02cdd38d493f7870e205
saeedhei <saeedhei@gmail.com>
2025-01-30 11:22:34 +0100

    env added

--- backend-telegram-js/bot/index.js
+++ backend-telegram-js/bot/index.js
@@ -1,7 +1,14 @@
+require('dotenv').config()
+
+const { BOT_TOKEN } = process.env;
 const TelegramBot = require('node-telegram-bot-api');
-const token = '7231037333:AAHjaVgDGhJ9nKG9_OniRpHxsH6con_71LI';
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(BOT_TOKEN, {polling: true});
+
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, 'Received your message');
 });
+
+bot.onText(/^[/]start$/, async (msg) => { 
+  await bot.sendMessage(msg.chat.id, `Hi.`);
+ });
