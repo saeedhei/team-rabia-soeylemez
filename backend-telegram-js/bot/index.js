a5f0f83fcf46d3d7292b91606177aec9bb7ce81d
saeedhei <saeedhei@gmail.com>
2025-02-01 11:15:35 +0100

    keyboards added

--- backend-telegram-js/bot/index.js
+++ backend-telegram-js/bot/index.js
@@ -4,11 +4,22 @@ const { BOT_TOKEN } = process.env;
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(BOT_TOKEN, {polling: true});
 
+const StartKeyboard = [
+  ['🇩🇪 German', '🇺🇸 English']
+];
+
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Received your message');
+  // bot.sendMessage(chatId, 'Received your message');
 });
 
 bot.onText(/^[/]start$/, async (msg) => { 
-  await bot.sendMessage(msg.chat.id, `Hi.`);
+  const opts = {
+    reply_markup: JSON.stringify({
+      keyboard: StartKeyboard,
+      resize_keyboard: true,
+      one_time_keyboard: true,
+    }),
+  };
+  await bot.sendMessage(msg.chat.id, `Hi ${msg.from.first_name} 😊\n\nWelcome to our Telegram bot! ❤️❤️❤️\nPlease select a menu:`, opts);
  });
