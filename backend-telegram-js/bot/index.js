e64885b558c24dd493607b73d1792f3a9057ccf5
rabiasoeyle <ing.rabia.soeylemez@gmail.com>
2025-02-04 11:16:18 +0100

    <feat>:<sendMessage after recieved german or english language>

--- backend-telegram-js/bot/index.js
+++ backend-telegram-js/bot/index.js
@@ -3,15 +3,27 @@ require('dotenv').config()
 const { BOT_TOKEN } = process.env;
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(BOT_TOKEN, {polling: true});
-
+const userLanguages = {};
 const StartKeyboard = [
   ['🇩🇪 German', '🇺🇸 English']
 ];
+const messages = {
+  en: {
+    welcome: "Welcome to our Telegram bot! ❤️❤️❤️\nPlease select a menu:",
+    selected_language: "You have selected English 🇺🇸",
+    response: "Hello! How can I help you?",
+  },
+  de: {
+    welcome: "Willkommen bei unserem Telegram-Bot! ❤️❤️❤️\nBitte wähle ein Menü:",
+    selected_language: "Du hast Deutsch 🇩🇪 ausgewählt",
+    response: "Hallo! Wie kann ich dir helfen?",
+  },
+};
 
-// bot.on('message', (msg) => {
-//   const chatId = msg.chat.id;
-//   bot.sendMessage(chatId, 'Received your message');
-// });
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  // bot.sendMessage(chatId, 'Received your message');
+});
 
 bot.onText(/^[/]start$/, async (msg) => { 
   const opts = {
@@ -25,8 +37,11 @@ bot.onText(/^[/]start$/, async (msg) => {
  });
 
  bot.onText('🇩🇪 German', async (msg)=>{
-  await bot.sendMessage(msg.chat.id, `Hallo, was möchtest du mich fragen?`);
+  userLanguages[chatId] = 'de'; // Speichere Sprache als Deutsch
+  await bot.sendMessage(msg.chat.id, messages.userLanguages[chatId].selected_languages);
  })
  bot.onText('🇺🇸 English', async (msg)=>{
+  userLanguages[chatId] = 'en'; // Speichere Sprache als Englisch
   await bot.sendMessage(msg.chat.id, `Hello, what do you want to ask me?`);
  })
+
